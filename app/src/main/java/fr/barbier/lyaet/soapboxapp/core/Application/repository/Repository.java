389e83e216cab36f471b75abfc083126e2bc71bf
Louis Barbier <louis.barbier41@outlook.fr>
2022-06-09 15:40:51 +0200

    Created repository and added helper for database

---
+++ app/src/main/java/fr/barbier/lyaet/soapboxapp/core/Application/repository/Repository.java
@@ -0,0 +1,13 @@
+package fr.barbier.lyaet.soapboxapp.core.Application.repository;
+
+import com.j256.ormlite.dao.Dao;
+
+import fr.barbier.lyaet.soapboxapp.core.Application.Business.BasicModel;
+
+public abstract class Repository<T extends BasicModel> {
+    protected Dao<T, Integer> dao;
+
+    public Dao<T, Integer> getDao() {
+        return this.dao;
+    }
+}
