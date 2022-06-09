e2ffbc90048aac0ac8a16d7d301b5be31fea424d
syukoGit <syukodev@outlook.com>
2022-06-09 13:34:35 +0200

    init
    Signed-off-by: syukoGit <syukodev@outlook.com>

---
+++ app/src/main/java/fr/barbier/lyaet/soapboxapp/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.barbier.lyaet.soapboxapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/fr/barbier/lyaet/soapboxapp/package-info.java
@@ -0,0 +1,1 @@
+package fr.barbier.lyaet.soapboxapp;
