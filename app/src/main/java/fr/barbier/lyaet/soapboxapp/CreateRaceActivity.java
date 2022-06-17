b2cde507bcb2ce6cf938de0b37c24050ff7d223b
syukoGit <syukodev@outlook.com>
2022-06-17 11:59:06 +0200

    Bug fix
    Signed-off-by: syukoGit <syukodev@outlook.com>

--- app/src/main/java/fr/barbier/lyaet/soapboxapp/CreateRaceActivity.java
+++ app/src/main/java/fr/barbier/lyaet/soapboxapp/CreateRaceActivity.java
@@ -1,7 +1,5 @@
 package fr.barbier.lyaet.soapboxapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -9,54 +7,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.maps.MapView;
-
-import java.time.Clock;
-import java.time.Instant;
-import java.util.Date;
-
 import fr.barbier.lyaet.soapboxapp.core.Application.Business.RaceModel;
 import fr.barbier.lyaet.soapboxapp.core.Application.repository.RaceRepository;
 import fr.barbier.lyaet.soapboxapp.location.service.LocationService;
 import fr.barbier.lyaet.soapboxapp.location.service.TextEditLocationUpdater;
 
+import java.util.Date;
+
 public class CreateRaceActivity extends AppCompatActivity {
+
+    private EditText addressEditText;
+
+    private CalendarView calendarView;
+
+    private Button createRaceButton;
 
     private MapView mapView;
 
     private EditText raceNameTextEdit;
 
-    private Button createRaceButton;
-
-    private CalendarView calendarView;
-
-    private EditText addressEditText;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_race);
-        this.raceNameTextEdit = this.findViewById(R.id.courseNameEditText);
-        this.createRaceButton = this.findViewById(R.id.createRaceButton);
-        this.calendarView = this.findViewById(R.id.calendarView);
-        this.addressEditText = this.findViewById(R.id.addressEditText);
-        Geocoder geocoder = new Geocoder(this.getApplicationContext());
-        TextEditLocationUpdater textEditLocationUpdater = new TextEditLocationUpdater(this.addressEditText,geocoder);
-        LocationService locationService = new LocationService(this, textEditLocationUpdater);
-        locationService.requestForLocation();
-
+    public void cancelCreate(View view) {
+        this.returnToList();
     }
 
-    public void createRace(View view)
-    {
-        if(this.raceNameTextEdit.getText().toString().isEmpty())
-        {
+    public void createRace(View view) {
+        if (this.raceNameTextEdit.getText().toString().isEmpty()) {
             return;
         }
 
         Date date = new Date(this.calendarView.getDate());
-        RaceModel raceModel = RaceModel.create(this.raceNameTextEdit.getText().toString(), date, this.addressEditText.getText().toString());
+        RaceModel raceModel = RaceModel.create(this.raceNameTextEdit.getText()
+                                                                    .toString(), date, this.addressEditText.getText()
+                                                                                                           .toString());
 
         Thread createRaceThread = new Thread(new Runnable() {
 
@@ -69,14 +53,22 @@ public class CreateRaceActivity extends AppCompatActivity {
         this.returnToList();
     }
 
-    public void cancelCreate(View view)
-    {
-        this.returnToList();
-    }
-
-    public void returnToList()
-    {
+    public void returnToList() {
         Intent intent = new Intent(this, RaceListActivity.class);
         this.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_create_race);
+        this.raceNameTextEdit = this.findViewById(R.id.courseNameEditText);
+        this.createRaceButton = this.findViewById(R.id.btnCreateRace);
+        this.calendarView = this.findViewById(R.id.calendarView);
+        this.addressEditText = this.findViewById(R.id.addressEditText);
+        Geocoder geocoder = new Geocoder(this.getApplicationContext());
+        TextEditLocationUpdater textEditLocationUpdater = new TextEditLocationUpdater(this.addressEditText, geocoder);
+        LocationService locationService = new LocationService(this, textEditLocationUpdater);
+        locationService.requestForLocation();
     }
 }
